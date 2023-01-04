2ef68de86ebdebe6d466ba1f093bdfbb6607a858
megaurso <bernadogui@hotmail.com>
2023-01-04 18:05:31 -0300

    feat: create header without style

--- src/pages/Players/Players.tsx
+++ src/pages/Players/Players.tsx
@@ -1,5 +1,9 @@
+import { Header } from "components/Header/Header"
+
 export function Players () {
     return (
-        <div>Players</div>
+        <div>
+            <Header/>
+        </div>
     )
 }
