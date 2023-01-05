d2fcbae1fe15777da27e3d84eb215143060a0cdf
Mauí Buarque <maui.buarque1@gmail.com>
2023-01-05 16:46:12 -0300

    change profile picture

--- src/contexts/interfaces.ts
+++ src/contexts/interfaces.ts
@@ -1,25 +1,30 @@
 import React from "react";
 
 export interface iContextChildrenProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export interface iUserProviderValue {
-    login: (data: iUserLogin) => Promise<void>;
-    players: iPlayers[]
+  login: (data: iUserLogin) => Promise<void>;
+  players: iPlayers[];
+  user: iPlayers | undefined;
+  setUser: React.Dispatch<React.SetStateAction<iPlayers | undefined>>;
+  loadUser: () => void;
 }
 
-export interface iUserLogin{
-    email: string;
-    password: string;
+export interface iUserLogin {
+  email: string;
+  password: string;
 }
 
-export interface iPlayers{
-    email:string;
-    nickname:string;
-    elo:string;
-    route:string:
-    id:number:
-    main:string:
-    bio:string
+export interface iPlayers {
+  email: string;
+  nickname: string;
+  elo: string;
+  route: string;
+  id: number;
+  main?: string;
+  bio?: string;
+  profileIcon?: string;
+  discord?: string;
 }
