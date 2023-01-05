2f424bd97afefa02bb74f38607692d5a61b96c20
megaurso <bernadogui@hotmail.com>
2023-01-05 14:02:56 -0300

    fix fragments

--- src/pages/Players/Players.tsx
+++ src/pages/Players/Players.tsx
@@ -1,4 +1,3 @@
-
 import { UserCard } from "components/UserCard/UserCard"
 import { Header } from "components/Header/Header"
 import { UserContext } from "contexts/UserContext"
@@ -11,49 +10,51 @@ export function Players () {
 
 
     return (
-        <UserCard />
-        <div>
-            <Header/>
-            <section>
-                <input type="text" placeholder="Pesquisar usuário" />
-                <div>
-                    <select>
-                        <option value="">Rota</option>
-                    </select>
-                    <select>
-                        <option value="">Elo</option>
-                    </select>
-                </div>
-            </section>
-            <main>
-                <ul>
-                    {players.map((elem)=>(
-                        <li key={elem.id}>
-                            <h2>{elem.nickname}</h2>
-                            <h2>{elem.route}</h2>
-                            <h2>{elem.elo}</h2>
-                            <h2>Main {elem.main}</h2>
-                            <h2>{elem.bio}</h2>
-                        </li>
-                    ))}
-                </ul>
-            </main>
-            <footer>
-                <div>
+        <>
+            <UserCard />
+            <div>
+                <Header/>
+                <section>
+                    <input type="text" placeholder="Pesquisar usuário" />
                     <div>
-                        <img src={logo} alt="Logo" />
-                        <p>Feito com ❤️ por alunos da Kenzie Academy Brasil</p>
+                        <select>
+                            <option value="">Rota</option>
+                        </select>
+                        <select>
+                            <option value="">Elo</option>
+                        </select>
                     </div>
+                </section>
+                <main>
+                    <ul>
+                        {players.map((elem)=>(
+                            <li key={elem.id}>
+                                <h2>{elem.nickname}</h2>
+                                <h2>{elem.route}</h2>
+                                <h2>{elem.elo}</h2>
+                                <h2>Main {elem.main}</h2>
+                                <h2>{elem.bio}</h2>
+                            </li>
+                        ))}
+                    </ul>
+                </main>
+                <footer>
                     <div>
-                        <a target="_blank" href="">Gabriel Galvao Pavao - PO</a>
-                        <a target="_blank" href="https://www.linkedin.com/in/bernardo-guimaraes-59aa6320a/">Bernardo Guimaraes Oliveira Nogueira - SM</a>
-                        <a target="_blank" href="https://www.linkedin.com/in/vin%C3%ADcius-lira-a6294b244/">Carlos Vinícius Lira Moreira - QA</a>
-                        <a target="_blank" href="https://www.linkedin.com/in/typhonn003/">Diego Lima - QA</a>
-                        <a target="_blank" href="https://www.linkedin.com/in/mauibuarque/">Maui Andrade Buarque - QA</a>
-                        <a target="_blank" href="">Kaique Amorim Vasconcelos Da Silva - QA</a>
+                        <div>
+                            <img src={logo} alt="Logo" />
+                            <p>Feito com ❤️ por alunos da Kenzie Academy Brasil</p>
+                        </div>
+                        <div>
+                            <a target="_blank" href="">Gabriel Galvao Pavao - PO</a>
+                            <a target="_blank" href="https://www.linkedin.com/in/bernardo-guimaraes-59aa6320a/">Bernardo Guimaraes Oliveira Nogueira - SM</a>
+                            <a target="_blank" href="https://www.linkedin.com/in/vin%C3%ADcius-lira-a6294b244/">Carlos Vinícius Lira Moreira - QA</a>
+                            <a target="_blank" href="https://www.linkedin.com/in/typhonn003/">Diego Lima - QA</a>
+                            <a target="_blank" href="https://www.linkedin.com/in/mauibuarque/">Maui Andrade Buarque - QA</a>
+                            <a target="_blank" href="">Kaique Amorim Vasconcelos Da Silva - QA</a>
+                        </div>
                     </div>
-                </div>
-            </footer>
-        </div>
+                </footer>
+            </div>
+        </>
     )
 }
