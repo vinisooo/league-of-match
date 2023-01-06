85dcf678fe293c5489a58819f5aa79a1631fea04
vinisooo <viniciuslira9397@gmail.com>
2023-01-06 18:02:00 -0400

    feat: add conditional rendering at header and add logout function

--- src/components/Header/NavList/NavList.tsx
+++ src/components/Header/NavList/NavList.tsx
@@ -1,11 +1,19 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { StyledNavList } from "./StyledNavList";
 
 export function NavList () {
-    const location = useLocation()
-    console.log(location);
-    
-    
+    const location = useLocation();
+
+    const navigate = useNavigate();
+
+    const loggedUser = localStorage.getItem("@league-of-match: logged-user");
+
+    function logOut(){
+        localStorage.removeItem("@league-of-match: id");
+        localStorage.removeItem("@league-of-match: token");
+        localStorage.removeItem("@league-of-match: logged-user");
+    }
+
     return (
         <StyledNavList>
             <ul>
@@ -16,14 +24,28 @@ export function NavList () {
                 <li>
                     <Link to={"/players"} className={location.pathname === "/players" ? "emphasis" : ""}>Players</Link>
                 </li>
+                {
+                    loggedUser?
+                    <>
+                        <li>
+                            <Link to={"/myprofile"} className={location.pathname === "/myprofile" ? "emphasis" : ""}>Meu Perfil</Link>
+                        </li>
 
-                <li>
-                    <Link to={"/myprofile"} className={location.pathname === "/myprofile" ? "emphasis" : ""}>Meu Perfil</Link>
-                </li>
-
-                <li>
-                    <Link to={"/login"}>Sair</Link>
-                </li>
+                        <li>
+                            <Link to={"/login"} onClick={logOut}>Sair</Link>
+                        </li>
+                    </>
+                    :
+                    <>
+                        <li>
+                                <Link to={"/register"} className={location.pathname === "/register" ? "emphasis" : ""}>Registrar</Link>
+                        </li>
+                        <li>
+                                <Link to={"/login"} className={location.pathname === "/register" ? "emphasis" : ""}>Logar</Link>
+                        </li>
+                    </>
+                    
+                }
             </ul>
         </StyledNavList>
     )
