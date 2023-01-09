9f665da03143c96d31647df7c055a33762bd52a4
Kaique Amorim <106697668+KaiqueVasc@users.noreply.github.com>
2023-01-09 12:29:45 -0300

    fix noValidate

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,25 +1,27 @@
 import logo from "../../assets/logo.svg";
 import { StyledForm } from "./StyledForm";
 
-interface iFormProps{
-    children: React.ReactNode;
-    left?: boolean;
-    formTitle?: string;
-    callback: ()=>void;
+interface iFormProps {
+  children: React.ReactNode;
+  left?: boolean;
+  formTitle?: string;
+  callback: () => void;
 }
 
-export function Form({children,formTitle, left, callback}:iFormProps){
-
-    return(
-        <StyledForm onSubmit={callback}>
-            <img className={left ? "align-img-left": ""} src={logo} alt="league of match" />
-            <div>
-                <header className={left ? "align-title-left": ""}>
-                    <h1>{formTitle}</h1>
-                </header>
-                {children}
-            </div>
-        </StyledForm>
-    )
-
+export function Form({ children, formTitle, left, callback }: iFormProps) {
+  return (
+    <StyledForm noValidate onSubmit={callback}>
+      <img
+        className={left ? "align-img-left" : ""}
+        src={logo}
+        alt="league of match"
+      />
+      <div>
+        <header className={left ? "align-title-left" : ""}>
+          <h1>{formTitle}</h1>
+        </header>
+        {children}
+      </div>
+    </StyledForm>
+  );
 }
