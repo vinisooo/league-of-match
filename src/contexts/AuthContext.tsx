0defbd8cdf9db5f5beba7f72a8cfb8713f80e8b6
Gabriel <gabpavao@outlook.com>
2023-01-11 16:30:23 -0300

    fix: patch request

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,20 +1,27 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { api } from "services/api";
 import { iAuthProviderValue, iContextChildrenProps } from "./interfaces";
+import { UserContext } from "./UserContext";
 
 export const AuthContext = createContext({} as iAuthProviderValue)
 
 export function AuthProvider ({ children }: iContextChildrenProps) {
-  const [loading, setLoading] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const { setUser } = useContext(UserContext)
+    const [loading, setLoading] = useState(true);
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
 
     async function authenticateUserToken () {
         const token = localStorage.getItem("@league-of-match: token");
+        const userId = localStorage.getItem("@league-of-match: id");
 
         try {
-            api.defaults.headers.common.authorization = `Bearer ${token}`;
-            await api.get(`/auth`);
+            const { data } = await api.patch(`/users/${userId}`, {}, {
+                headers: {
+                  authorization: `Bearer ${token}`
+                }
+            });
             setIsAuthenticated(true)
+            setUser(data)
         } catch (error) {
             console.log(error)
             setIsAuthenticated(false)
